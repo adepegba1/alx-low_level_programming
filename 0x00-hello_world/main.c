43e9db175f2a7ec48675153b25cc9628ae1ebe98
root <adepegbadavid@rocketmail.com>
2022-09-08 03:40:30 -0700

    hello world

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-int main (void)
-{	printf ("hello world\n");
-	return (0);
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("Holberton School\n");
+
+			return (0);
+
 }
