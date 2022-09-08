4e6af19e94ef9567bffe4b89ee227fc770ab1daf
root <adepegbadavid@rocketmail.com>
2022-09-08 12:00:03 -0700

    size of btyes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,11 @@
  * Return: 0
  */
 int main(void)
-{	int i;
-	char c;
-	float f;
-	double d;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(i));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
