4f1974470416918316e4c17f451032cd8abc90e7
root <adepegbadavid@rocketmail.com>
2022-09-08 06:22:03 -0700

    printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * decription: print with proper grammar, but the outcome is a piece of art,
+ * retun: 0
+ */
+
+int main (void)
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
