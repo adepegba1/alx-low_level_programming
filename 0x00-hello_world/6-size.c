600624374e3dbabd1f6b24989442ee37c9fe0011
root <adepegbadavid@rocketmail.com>
2022-09-08 09:51:15 -0700

    size of bytes on computer

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: printing the size of byte on each computer
+ * Return: 0
+ */
+int main(void)
+{	int i;
+	char c;
+	float f;
+	double d;
+
+	printf("size of a char: %lu byte(s)\n", (unsigned long) sizeof(c));
+	printf("size of a int: %lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long) sizeof(i));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long) sizeof(f));
+	return (0);
+}
