82b47154be24edfd9c3366530f20ed98470b7db8
root <adepegbadavid@rocketmail.com>
2022-09-09 10:47:54 -0700

    base 10 number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: numbers inn base 10
+ * Return: 0
+ */
+int main(void)
+{	int a;
+	for (a = 0; a < 10; a++)
+	{	printf("%d", a); }
+		printf("\n");
+}
