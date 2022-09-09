a8f5d3fef5c07742a6bd6621a7aaf49703e3d216
root <adepegbadavid@rocketmail.com>
2022-09-09 12:26:45 -0700

    dont

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: base 16 number
+ * Return: 0
+ */
+int main(void)
+{	int num;
+	char ch;
+	for (num = 0; num < 10; num++)
+	{ putchar((num % 10) + '0'); }
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{ putchar(ch); }
+	putchar('\n');
+	return (0);
+}
