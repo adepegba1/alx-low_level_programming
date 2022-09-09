076d1ecde95a9477730b5bfb3f57d967670ee36f
root <adepegbadavid@rocketmail.com>
2022-09-09 14:36:29 -0700

    Hexadecimal number

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,8 @@
 int main(void)
 {	int num;
 	char ch;
-	for (num = 0; num < 10; num++)
+
+	for (num = 0; num <= 9; num++)
 	{ putchar((num % 10) + '0'); }
 	for (ch = 'a'; ch <= 'f'; ch++)
 	{ putchar(ch); }
