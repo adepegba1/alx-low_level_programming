c4e84dd48d247ff4d5615645203649892f2f9736
root <adepegbadavid@rocketmail.com>
2022-09-09 10:36:46 -0700

    Alphabet no e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Description: all alphabet except e and q
+ *Return: 0
+ */
+
+int main(void)
+{	char low, e, q;
+	e = 'e';
+	q = 'q';
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != 'e' && low != 'q')
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
