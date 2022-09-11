1e8649e1a1cdcae00659067cca317fae54402912
root <adepegbadavid@rocketmail.com>
2022-09-11 08:11:56 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * Description: prints "\"Programming is like building a multilingual puzzle"
+ * description: prints "\"Programming is like building a multilingual puzzle"
  * Return: 0
  */
 
- int main(void)
+int main(void)
 {
-               puts("\"Programming is like building a multilingual puzzle");
-	return (0);    
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
