bdeb81fe608257f62bf847f1e9a409afba5b7bd6
root <adepegbadavid@rocketmail.com>
2022-09-13 03:54:46 -0700

    trying

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Description: alphabet function
+ * Return: void
+ */
+void print_alphabet(void)
+{	int b;
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		putchar(b);
+	}
+}
+int main(void)
+{	print_alphabet();
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,9 @@
+/*#ifndef _main_h_
+#define _main_h_
+
+void print_alphabet(void);
+
+#endif
+*/
 #include <stdio.h>
 #include <unistd.h>
