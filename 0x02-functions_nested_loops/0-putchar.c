bc4efe790c5cb107a1081721bb2e0913614adfdc
root <adepegbadavid@rocketmail.com>
2022-09-13 10:07:32 -0700

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,15 @@
 #include "main.h"
-/**
- * main - Entry point
- * Description: using _putchar
- * Return: 0
- */
-int _putchar(char c)
-{
-	char word[8] = "_putchar";
-	int i;
-	for (i = 1; i <=8; i++)
-	
-	{
-	       	_putchar(word[i]);
-	}
+
+int main(void)
+{	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
