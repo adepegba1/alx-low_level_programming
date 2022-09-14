b21ce9957e533b8b488c56d8dfc40693c358be86
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2022-09-14 19:31:22 +0300

    Update main.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -9,5 +9,17 @@
 int main(void)
 {
 	print_alphabet();
+	print_alphabet_x10(void);
+	_islower(int c);
+	_isalpha(int c);
+	print_sign(int n);
+	_abs(int);
+	print_last_digit(int);
+	jack_bauer(void);
+	times_table(void);
+	add(int, int);
+	print_to_98(int n);
+	print_times_table(int n)
+	
 	return (0);
 }
