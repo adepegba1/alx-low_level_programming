3de3618111e3a2ca944d265dcdd977d8e598cb17
root <adepegbadavid@rocketmail.com>
2022-09-15 10:58:23 -0700

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,12 +7,17 @@
  */
 void print_line(int n)
 {
-	if (n > 0)
+	int i;
+
+	for (i = '1'; i <= 'n'; i++)
 	{
-		_putchar('_');
-	}
-	else
-	{
-		_putchar('\n');
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			_putchar('\n');
+		}
 	}
 }
