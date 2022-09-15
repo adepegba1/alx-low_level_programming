75747c7b68b395839f0efe2a3abb4b158247675b
root <adepegbadavid@rocketmail.com>
2022-09-15 05:39:18 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * print_numbers - print number
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return ();
+}
