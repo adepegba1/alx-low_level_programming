8dc047621b7103c31447e7646b9fe42907516453
root <adepegbadavid@rocketmail.com>
2022-09-15 08:19:56 -0700

    print line'

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print a line
+ * @n: number of interger
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n <= '0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('_');
+	}
+	return (n);
+}
