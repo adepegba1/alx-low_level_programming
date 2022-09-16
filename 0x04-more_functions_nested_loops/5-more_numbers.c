42b32064a85fa4722d6cfc650ba09b60a9c22808
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2022-09-16 16:12:50 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,9 @@ void more_numbers(void)
 		for (j = 0; j < 15; j++)
 		{
 			if (j > 9)
-			{
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
-			}
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
