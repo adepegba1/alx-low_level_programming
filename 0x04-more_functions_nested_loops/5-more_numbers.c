e6b5deb0b5d43e83b55baf5b3c2d391f94b9053f
root <adepegbadavid@rocketmail.com>
2022-09-16 00:05:26 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,11 +9,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			_putchar('j');
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
