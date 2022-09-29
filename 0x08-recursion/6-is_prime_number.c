63523d4d0662a5f00739352bce7142686788849c
root <adepegbadavid@rocketmail.com>
2022-09-29 06:07:37 -0700

    prinme number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  *is_prime_number - prime number
+  *
+  *@n: value to check
+  *
+  *Return
+  */
+int primeCheck(int i, int j);
+int is_prime_number(int n)
+{
+	if (n == 2)
+		return (1);
+	return (primeCheck(2, n));
+}
+
+/**
+  *primeCheck - checking if the number is prime number
+  *
+  *@i: factor check
+  *
+  *@j: possible prime
+  *
+  *Return: 1 if prime and 0 otherwise
+  */
+int primeCheck(int i, int j)
+{
+	if (j < 2 || j % i == 0)
+		return (0);
+	else if (i > j / 2)
+		return (1);
+	else
+		return (primeCheck(i + 1, j));
+}
