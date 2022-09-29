4104b261c4f6e35a87b8884729839ee05d9e6286
root <adepegbadavid@rocketmail.com>
2022-09-29 03:38:26 -0700

    x raised to power y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,10 +3,19 @@
 /**
   *_pow_recursion - power of number function
   *
-  *@x: the number entered
+  *@x: the number to raise to power
   *
   *@y: the power nmuber
   *
-  *Return: the answer
+  *Return: x raise to power of y
   */
 
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x *  _pow_recursion(x, y - 1));
+}
