fb6859ed488aadf614d44de34f4e31ebcc8897c7
root <adepegbadavid@rocketmail.com>
2022-09-30 01:16:35 -0700

    a program that prints the number of arguments passed into it.

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,6 +7,10 @@
   *If you rename the program, it will print the new name, without having
   *to compile it again
   *
+  *@argc: argument count
+  *
+  *@argv: argument vector (arrays of string)
+  *
   *Return: always 0
   */
 int main(int argc, char *argv[])
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Description: program that prints the number of arguments passed into it
+  *
+  *@argc: argument count
+  *
+  *@argv: argument vector (arrays of stings)
+  *Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
