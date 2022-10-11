a831b2b14eb9deb575cf0203bfa8934f6899f837
root <adepegbadavid@rocketmail.com>
2022-10-11 03:19:50 -0700

    all

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+  *new_dog -  a function that creates a new dog.
+  *@name:
+  *@age:
+  *@owner:
+  *
+dog_t *new_dog(char *name, float age, char *owner);
