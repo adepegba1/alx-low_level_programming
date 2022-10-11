0e946d340a12ba783523273a670c628c7f99cbdb
root <adepegbadavid@rocketmail.com>
2022-10-11 02:16:57 -0700

    structure of Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,20 @@
+#ifndef _dog_h
+#define _dog_h
+/**
+  *struct dog - structure of a Dog
+  *
+  *@name: name of Dog
+  *
+  *@age: age of the Dog
+  *
+  *@owner: the owner of the Dog
+  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
 
