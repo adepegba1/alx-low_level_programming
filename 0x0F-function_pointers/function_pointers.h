28e61aa0ec8a76bc7e3fed2db3408ddd993d4e8a
root <adepegbadavid@rocketmail.com>
2022-10-12 03:23:51 -0700

    header files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
-#ifndef _main_h
-#define _main_h
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
+#include <string.h>
+#include <stdlib.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
