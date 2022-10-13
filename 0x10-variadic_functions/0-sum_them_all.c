b4d6b805c066c1b31371982ba1cd4e2537911c18
root <adepegbadavid@rocketmail.com>
2022-10-13 07:23:36 -0700

    a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,17 +16,17 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(ptr, n);
 
-	if (n == 0)
+	for (i = 0; i < n; i++)
 	{
-		return (0);
-	}
-	else
-	{
-		for (i = 0; i < n; i++)
+		if (n == 0)
+		{
+			return (0);
+		}
+		else
 		{
 			sum = sum + va_arg(ptr, int);
 		}
+	}
 		va_end(ptr);
 		return (sum);
-	}
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  *print_numbers - a function that prints numbers, followed by a new line
+  *
+  *@separator: separater the number
+  *@n: number entred
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+}
