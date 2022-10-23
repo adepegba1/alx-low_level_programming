837d733507c86791c3d2347ded551c9a564da4ac
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2022-10-23 21:00:34 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,7 +4,18 @@
  * Description: number follow by comma
  * Return: 0
  */
-
+/*
+int main(void)
+{	int ch;
+	for (ch = '0'; ch <= '9'; ch++)
+	{ putchar(ch);
+	if (ch != '9')
+	{ putchar(',');
+	 putchar(' '); }}
+	putchar('\n');
+	return (0);
+}
+*/
 
 int main(void)
 {
