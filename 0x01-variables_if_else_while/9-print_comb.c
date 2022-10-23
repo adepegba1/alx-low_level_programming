2699262908f1be86c6dd59d83764ab88bfe128aa
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2022-10-23 20:57:16 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,12 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar((i % 10) + '0');
-		putchar(' ');
 		if (i != 9)
 		{
+			putchar(' ');
 			putchar(',');
 		}
 	}
+	putchar(' ');
 	return (0);
 }
