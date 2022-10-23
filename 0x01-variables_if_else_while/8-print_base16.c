f8cf8a04920d8df5479da44ceeac8b8060bb58cd
Adepegba David Ayobami <110990491+adepegba1@users.noreply.github.com>
2022-10-23 20:33:26 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,3 +15,30 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
+/*
+/**
+ * main - Entry point
+ * Description: base 16 number
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main() {
+   int i;
+   int a = 65;
+   
+   for(i = 0; i < 16; i++)
+   {
+        if (i > 9)
+       {
+           putchar(a);
+           a++;
+       }
+      else
+      putchar((i % 10) + '0');
+   }
+ putchar(10);
+    return 0;
+}
+*/
