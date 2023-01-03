e2a29a50e7cfe124855a7b52f2743bfa1d64d260
adepegba1 <adepegbadavid@rocketmail.com>
2023-01-03 05:40:36 -0800

    redo task

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,15 +8,11 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i = i + 2)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
